0ca15762472e4631756fb8a4b11f4093c8149653
A. Kim <kingalvinjkim@gmail.com>
2023-08-20 15:37:04 +0900

    fixed endscreen buttons

--- Assets/Scripts/Level/LevelFinisher.cs
+++ Assets/Scripts/Level/LevelFinisher.cs
@@ -12,7 +12,7 @@ public class LevelFinisher : MonoBehaviour
     {
         if (!IsPlayer(other)) return;
 
-        Time.timeScale = 0;
+        // Time.timeScale = 0;
         var deathText = _endScreen.transform.Find("Level End Message");
         var tmpText = deathText.GetComponent<TMP_Text>();
 
@@ -22,12 +22,15 @@ public class LevelFinisher : MonoBehaviour
         if (trashScript.TrashCount == 0) tmpText.text = "Level finished with no trash collected!";
         else if (trashScript.TrashCount == 1) tmpText.text = "Level finished with " + trashScript.TrashCount + " piece of trash collected!";
         else tmpText.text = "Level finished with " + trashScript.TrashCount + " pieces of trash collected!";
-        _energyBar.SetActive(false);
-        _trashCounter.SetActive(false);
+        // _energyBar.SetActive(false);
+        // _trashCounter.SetActive(false);
         _endScreen.SetActive(true);
+
+        _endScreen.transform.SetSiblingIndex(_endScreen.transform.parent.childCount - 1);
     }
 
-    private bool IsPlayer(Collider2D other) {
+    private bool IsPlayer(Collider2D other)
+    {
         int layerOfCollider = other.gameObject.layer;
         string layerName = LayerMask.LayerToName(layerOfCollider);
         return layerName == "Player";
