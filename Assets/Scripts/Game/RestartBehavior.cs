966d6a253131e1881c34640e93f8f53157a4a4da
Finn Mattis <planto73@harkenpretty.com>
2023-08-20 00:55:19 -0400

    Made level finish pretty

--- Assets/Scripts/Game/RestartBehavior.cs
+++ Assets/Scripts/Game/RestartBehavior.cs
@@ -1,31 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RestartBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void RestartLevel()
     {
-        Debug.Log("restarting");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void ToMainMenu()
     {
-        Debug.Log("Going to main menu");
         SceneManager.LoadScene("Main Menu");
     }
 }
--- Assets/Scripts/Level/LevelFinisher.cs
+++ Assets/Scripts/Level/LevelFinisher.cs
@@ -1,14 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelFinisher : MonoBehaviour
 {
+    [SerializeField] private GameObject _energyBar;
+    [SerializeField] private GameObject _trashCounter;
     [SerializeField] private GameObject _endScreen;
+    [SerializeField] private GameObject _player;
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Time.timeScale = 0;
+        if (!IsPlayer(other)) return;
+
+        Time.timeScale = 0;
+        var deathText = _endScreen.transform.Find("Level End Message");
+        var tmpText = deathText.GetComponent<TMP_Text>();
+
+        var trashScript = _player.GetComponent<PlayerTrashPickup>();
+
+
+        if (trashScript.TrashCount == 0) tmpText.text = "Level finished with no trash collected!";
+        else if (trashScript.TrashCount == 1) tmpText.text = "Level finished with " + trashScript.TrashCount + " piece of trash collected!";
+        else tmpText.text = "Level finished with " + trashScript.TrashCount + " pieces of trash collected!";
+        _energyBar.SetActive(false);
+        _trashCounter.SetActive(false);
         _endScreen.SetActive(true);
+    }
+
+    private bool IsPlayer(Collider2D other) {
+        int layerOfCollider = other.gameObject.layer;
+        string layerName = LayerMask.LayerToName(layerOfCollider);
+        return layerName == "Player";
     }
 }
